9c928b319ae9fd92cc8e0a1edf8ef7a1efd2b1dd
k <keisangittens@gmail.com>
2018-07-30 20:06:14 -0400

    learnt about class bindings and using directives. fixed logic in script for listing elements on the DOM

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,39 +4,30 @@
   <ul>
   <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
   </ul>
+
+  <div v-bind:class="{ alert:!showAlert }"></div>
   </div>  
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Skills',
-  data(){
+  name: "Skills",
+  data() {
     return {
       skills: [
-        {"Skill": "Vue.js"},
-        {"Skill": "Frontend Developer"}
-      ]
-
-    }
+        { "skill": "Vue.js" }, { "skill": "Frontend Developer" }],
+        showAlert: true
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style  scoped>
+.alert{
+  background-color: yellow;
+  width: 100%;
+  height: 30px;
 }
 </style>
