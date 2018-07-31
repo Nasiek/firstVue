c5e5ebbbc4bd461584110c4d7526a8d3250865ff
k <keisangittens@gmail.com>
2018-07-30 23:02:53 -0400

    added v-validate for form. causing errorError: ENOENT: no such file or directory, open /Users/kl/Documents/Vue2018/vue-cdn/vue-skills/node_modules/node-libs-browser/node_modules/punycode/punycode.js

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Monsterrat:400,700');
+
 body {
-  background-color: #dddddd;
+  background-color: #EEEEEE;
+  font-family: 'Monsterrat',sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+
+body, html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  width: 50%;
 }
 </style>
